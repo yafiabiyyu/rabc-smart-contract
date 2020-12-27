ed3877db51335fa05a9ae3e589866f732ef34314
Abiyyu Yafi <luckyveri3@gmail.com>
2020-12-27 09:04:34 +0700

    write Petugas Testing

--- test/Petugas.test.js
+++ test/Petugas.test.js
@@ -3,36 +3,94 @@ const { constants,expectEvent, expectRevert } = require('@openzeppelin/test-help
 
 const Petugas = artifacts.require('Petugas');
 
-contract('Petugas', function([owner,petugas,other,pengguna]){
-    const DEFAULT_USER_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
-    const DEFAULT_PETUGAS_ROLE = '0x83741c3c887d330a865d1fce1157e1846b02421c0bc7c37f35286c5a35562936'
-
+contract('Petugas', function([owner,petugas,other]){
     beforeEach(async function(){
         this.petugas = await Petugas.new({from:owner});
     });
 
-    it('Owner menjadikan petugas sebagai admin pengguna', async function(){
-        this.petugas.setPetugasForRole(
-            DEFAULT_USER_ROLE,
-            DEFAULT_PETUGAS_ROLE,
-            {from:owner}
-        );
+    describe('[1.1] Owner Test Case', function(){
+        it('Owner menambahkan Petugas kedalam smart-contract', async function(){
+            const receipt = await this.petugas.addNewPetugas(
+                petugas,
+                {from:owner}
+            );
+            expectEvent(
+                receipt,
+                'NewPetugas',
+                {_newPetugasAddress:petugas}
+            );
+        });
+
+        it('Owner menghapus petugas dari smart-contract', async function(){
+            const receipt = await this.petugas.removePetugas(
+                petugas,
+                {from:owner}
+            );
+            expectEvent(
+                receipt,
+                'RemovePetugas',
+                {_removePetugasAdress:petugas}
+            );
+        });
     });
 
-    it('Non Owner menjadikan petugas sebagai admin pengguna', async function(){
-        await expectRevert(
-            this.petugas.setPetugasForRole(
-                DEFAULT_USER_ROLE,
-                DEFAULT_PETUGAS_ROLE,
-                {from:other}
-            ),
-            "Ownable: caller is not the owner"
-        );
-    })
-    // it('non-owner menamahkan admin',async function() {
-    //     await expectRevert(
-    //         this.petugas.AddPetugas(petugas,{from:other}),
-    //         "Ownable: caller is not the owner"
-    //     );
-    // });
+    describe('[1.2] Non Owner Test Case', function(){
+        it('Non Owner menambahkan petugas kedalam smart-contract', async function(){
+            await expectRevert(
+                this.petugas.addNewPetugas(
+                    petugas,
+                    {from:other}
+                ),
+                'Ownable: caller is not the owner'
+            );
+        });
+
+        it('Non Owner menghapus petugas dari smart-contract', async function(){
+            await expectRevert(
+                this.petugas.removePetugas(
+                    petugas,
+                    {from:other}
+                ),
+                'Ownable: caller is not the owner'
+            );
+        });
+    });
+});
+
+contract('Petugas', function([owner, petugas, member]){
+    beforeEach(async function(){
+        this.petugas = await Petugas.new({from:owner});
+        this.petugas.addNewPetugas(petugas,{from:owner});
+    });
+    describe('[1.1] Petugas Test Case', function(){
+        it('Petugas menambahkan member baru', async function(){
+            const receipt = await this.petugas.addNewMember(
+                member,
+                "yafiabiyyu",
+                {from:petugas}
+            );
+            expectEvent(
+                receipt,
+                'newMember',
+                {
+                    _memberAddress:member,
+                    _memberName:"yafiabiyyu"
+                }
+            );
+        });
+
+        it('Petugas menghapus member', async function(){
+            const receipt = await this.petugas.removeMember(
+                member,
+                {from:petugas}
+            );
+            expectEvent(
+                receipt,
+                'removeMembers',
+                {
+                    _memberAddress:member
+                }
+            );
+        });
+    });
 });
